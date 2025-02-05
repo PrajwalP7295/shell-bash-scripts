f71f5700c792a4161b41d29615351415b6d415d9
Prajwal Patil <ppatil7295@gmail.com>
2025-02-05 21:42:51 +0530

    Added resource util script as task-1

---
+++ README.md
@@ -0,0 +1,15 @@
+# SHELL SCRIPTS
+
+## [TASK-1](https://github.com/PrajwalP7295/shell-bash-scripts/blob/main/task-1.sh) : System Resource Monitoring Script
+
+This shell script provides a quick overview of the key system resources of a Linux server, including __MEMORY__, __SWAP__, __DISK USAGE__, and __PROCESS__ consumption.
+
+#### USAGE :-
+
+- Create the script file : 
+```nano resource_util.sh```
+- Provide executable permissions for the script :
+```chmod +x resource_util.sh```
+- Execute the script :
+```./resource_util.sh```
+
--- task-1.sh
+++ task-1.sh
@@ -1,33 +1,52 @@
-# script which prints date, current used RAM & the PID of the process utilizing highest RAM, each on new line in a file. Also set crontab to execute this script at 3:30 IST on last day of Jan , Feb, Nov excluding on the weekends.
+#!/bin/bash
 
-# File Output Ex:a
-# DD-MM-YYYY
-# RAM-XXXMB
-# PID-12345
+# Get Current DATE and TIME
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
 
+# Get TOTAL and USED RAM in MB
+TOTAL_RAM=$(free -m | awk 'NR==2 {print $2}')
+USED_RAM=$(free -m | awk 'NR==2 {print $3}')
+RAM_PERCENT=$(( 100 * USED_RAM / TOTAL_RAM ))  # RAM Usage Percentage
 
+# Get TOTAL and USED SWAP memory
+TOTAL_SWAP=$(free -m | awk 'NR==3 {print $2}')
+USED_SWAP=$(free -m | awk 'NR==3 {print $3}')
 
-#! /bin/bash
+# Get Disk USAGE 
+DISK_USAGE=$(df -h --output=source,size,used,avail,pcent /)
+DISK_HEADERS=$(echo "$DISK_USAGE" | head -n 1)  # Get headers
+DISK_DATA=$(echo "$DISK_USAGE" | tail -n +2)  # Get the data (excluding headers)
 
-DATE=$(date +%d-%m-%Y)
+# Get the TOP 5 MEMORY-consuming processes
+TOP_MEM_PROCESSES=$(ps -eo pid,%mem,comm --sort=-%mem | awk 'NR==2,NR==6 {printf "%-10s %-10s %-20s\n", $1, $2"%", $3}')
 
-echo "DATE = $DATE"
+# Get the TOP 5 CPU-consuming processes
+TOP_CPU_PROCESSES=$(ps -eo pid,%cpu,comm --sort=-%cpu | awk 'NR==2,NR==6 {printf "%-10s %-10s %-20s\n", $1, $2"%", $3}')
 
-RAM=$(free -mh | awk 'NR==2{printf "%i", $3}')
-
-echo "RAM = $RAM MB"
-
-PID=$(ps -eo pid,rss --sort=-rss | awk 'NR==2{print $1}')
-
-echo "PID = $PID"
-
-
-# Execution perm :    chmod +x a1.sh
-
-# Output : ./a1.sh
-
-# DATE = 13-02-2024
-# RAM = 1 MB
-# PID = 29940
-
-
+echo "----------------------------------------------------"
+echo "Date & Time:  $DATE"
+echo "----------------------------------------------------"
+echo "###### Memory Usage ######"
+echo "----------------------------------------------------"
+echo "Total RAM:    ${TOTAL_RAM}MB"
+echo "Used RAM:     ${USED_RAM}MB (${RAM_PERCENT}%)"
+echo "Total Swap:   ${TOTAL_SWAP}MB"
+echo "Used Swap:    ${USED_SWAP}MB"
+echo "----------------------------------------------------"
+echo "###### Disk Usage ######"
+echo "----------------------------------------------------"
+echo "$DISK_HEADERS"
+echo "$DISK_DATA"
+echo "----------------------------------------------------"
+echo "###### Top 5 Memory-Consuming Processes ######"
+echo "----------------------------------------------------"
+echo "PID        %MEM      Process Name"
+echo "----------------------------------------------------"
+echo "$TOP_MEM_PROCESSES"
+echo "----------------------------------------------------"
+echo "###### Top 5 CPU-Consuming Processes ######"
+echo "----------------------------------------------------"
+echo "PID        %CPU      Process Name"
+echo "----------------------------------------------------"
+echo "$TOP_CPU_PROCESSES"
+echo "----------------------------------------------------"
